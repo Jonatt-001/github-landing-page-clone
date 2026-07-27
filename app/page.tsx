af8a77b629277758d417404082b003d5230f0a6f
Jonatt-001 <1jonatttech@gmail.com>
2026-07-27 10:27:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,8 +38,8 @@ export default function Home() {
          <Globe/>
           <div className='max-w-[1280px] mx-auto relative z-[2]  px-5'>
             <ul className='lg:w-9/12 text-[14px] pt-16 text-[#7d8590] space-y-1'>
-              <li><sup id="footnote-1">1</sup> The Total Economic Impact™ Of GitHub Enterprise Cloud and Advanced Security, a commissioned study conducted by Forrester Consulting, 2022. Results are for a composite organization based on interviewed customers.</li>
-              <li><sup id="footnote-2">2</sup> GitHub, Octoverse 2022 The state of open source software.</li>
+              <li><sup id="footnote-1">1</sup> The Total Economic Impact™ Of GFox Git and Advanced Security. Results are for a composite organization based on interviewed customers.</li>
+              <li><sup id="footnote-2">2</sup> GFox Git: The state of open source software.</li>
             </ul>
           </div>
         </div>
